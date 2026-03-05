520d82edc55b05261c05d69b1c3b64391b231736
varun19424 <72430037+varun19424@users.noreply.github.com>
2026-03-05 15:14:37 +0530

    Update solution for mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -4,9 +4,13 @@ def mean_squared_error(y_pred, y_true):
     """
     Returns: float MSE
     """
-    # Write code here
-    y_pred_arr = np.array(y_pred)
-    y_true_arr = np.array(y_true)
-    if len(y_pred_arr) == len(y_true_arr):
-        return np.sum((y_pred_arr-y_true_arr)**2)/len(y_true_arr)
+    # y_pred = np.array(y_pred)
+    # y_true = np.array(y_true)
+    # # Write code here
+    # if len(y_pred) == len(y_true):
+    #     return (np.sum((y_pred-y_true)**2))/len(y_pred)
+    p = len(y_pred)
+    if len(y_pred) == len(y_true):
+        return sum([(n - m)*(n - m) for n , m in zip(y_pred ,y_true)])/p
+
     return None
